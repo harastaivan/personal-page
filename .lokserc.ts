a18824eaef27d0eccb4de980335dc51c867e87c5
Ivan Harasta <ivan.harasta@ackee.cz>
2022-08-02 16:16:48 +0200

    🔧 Configure lokse

--- .lokserc.ts
+++
@@ -1,9 +0,0 @@
-import { languages } from './src/modules/core/modules/localization/constants';
-
-export default {
-    sheetId: '1q8MSZGQqsWzo6zdme3ChwuWF7EdXMAyeZWSrgwOp-YQ',
-    dir: 'src/translations',
-    languages,
-    column: 'key_web',
-    format: 'json',
-};
---
+++ lokse.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    sheetId: '1EEjwW1TMb6nnEuY2GWzVhBOcE5YQW91PGIIB-oHGmAc',
+    dir: 'src/translations',
+    languages: ['cs', 'en'],
+    column: 'key_web',
+    format: 'json',
+    plugins: [],
+};
