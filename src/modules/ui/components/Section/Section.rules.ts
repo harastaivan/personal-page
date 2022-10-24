0200ad942d49c93c51a581a3477383e5d6cf55e9
Ivan Harasta <ivan.harasta@ackee.cz>
2022-10-24 21:24:43 +0200

    💄 Bigger padding bottom in Section

--- src/modules/ui/components/Section/Section.rules.ts
+++ src/modules/ui/components/Section/Section.rules.ts
@@ -2,5 +2,5 @@ import type { TRuleWithTheme } from 'styles/theme';
 
 export const section: TRuleWithTheme = ({ theme: { metrics } }) => ({
     paddingTop: metrics.spacing * 2,
-    paddingBottom: metrics.spacing * 2,
+    paddingBottom: metrics.spacing * 6,
 });
