01b6137d0cbe98b4e5a59563bd8051a23a1faa06
Ivan Harasta <ivan.harasta@ackee.cz>
2022-10-24 17:33:11 +0200

    🔥 Remove Loader ui component

--- src/modules/ui/components/index.ts
+++ src/modules/ui/components/index.ts
@@ -3,5 +3,4 @@ export * from './Dot';
 export * from './DottedItems';
 export * from './Headline';
 export * from './Link';
-export { default as Loader } from './Loader';
 export * from './Paragraph';
