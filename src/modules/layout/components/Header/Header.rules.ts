27597e5ad3c5dc34627401d1551751fd3bb2248e
Ivan Harasta <ivan.harasta@ackee.cz>
2022-10-24 22:02:26 +0200

    ✨ Add LanguageSwitcher to Header

--- src/modules/layout/components/Header/Header.rules.ts
+++ src/modules/layout/components/Header/Header.rules.ts
@@ -1,6 +1,10 @@
 import type { TRuleWithTheme } from 'styles/theme';
 
 export const header: TRuleWithTheme = ({ theme: { metrics } }) => ({
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+
     paddingTop: metrics.spacing * 5,
     paddingBottom: metrics.spacing * 5,
     paddingLeft: metrics.spacing * 15,
@@ -17,4 +21,8 @@ export const header: TRuleWithTheme = ({ theme: { metrics } }) => ({
         paddingLeft: metrics.spacing * 7,
         paddingRight: metrics.spacing * 7,
     },
+});
+
+export const headline: TRuleWithTheme = () => ({
+    marginBottom: 0,
 });
--- src/modules/layout/components/Header/Header.tsx
+++ src/modules/layout/components/Header/Header.tsx
@@ -3,6 +3,7 @@ import { FormattedMessage } from 'react-intl';
 import { useFelaEnhanced } from 'hooks';
 import type { RulesExtend } from 'styles/theme';
 import { SectionHeadline } from 'modules/ui';
+import { LanguageSwitcher } from 'modules/core/modules/localization';
 
 import * as felaRules from './Header.rules';
 
@@ -11,13 +12,14 @@ export interface HeaderProps {
 }
 
 export const Header = ({ extend }: HeaderProps) => {
-    const { styles } = useFelaEnhanced(felaRules, { extend });
+    const { styles, rules } = useFelaEnhanced(felaRules, { extend });
 
     return (
         <div className={styles.header}>
-            <SectionHeadline level={4} inverted>
+            <SectionHeadline level={4} inverted extend={{ headline: rules.headline }}>
                 <FormattedMessage id="header.title" />
             </SectionHeadline>
+            <LanguageSwitcher />
         </div>
     );
 };
