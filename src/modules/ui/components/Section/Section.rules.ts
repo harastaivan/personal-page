db6202ffa2a3b302e03c4bf670a6aaf13a04a315
Ivan Harasta <ivan.harasta@ackee.cz>
2022-11-13 12:49:35 +0100

    💄 Increase section padding

--- src/modules/ui/components/Section/Section.rules.ts
+++ src/modules/ui/components/Section/Section.rules.ts
@@ -1,6 +1,6 @@
 import type { TRuleWithTheme } from 'styles/theme';
 
 export const section: TRuleWithTheme = ({ theme: { metrics } }) => ({
-    paddingTop: metrics.spacing * 2,
-    paddingBottom: metrics.spacing * 6,
+    paddingTop: metrics.spacing * 4,
+    paddingBottom: metrics.spacing * 10,
 });
